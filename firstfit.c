cf9159bcd16e54192b7bf8435397b2f40434dbe9
AKHIL-DyC <akhiljee666@gmail.com>
2024-07-06 17:16:20 +0530

    fix

--- firstfit.c
+++ firstfit.c
@@ -1,31 +1,37 @@
 #include<stdio.h>
 #include<string.h>
-void firstfit(int blockSize[],int processSize[],int n){
-    int allocation[n];
-    memset(allocation,-1,sizeof(allocation));
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(blockSize[j]>processSize[i]){
+void firstfit(int blockSize[],int processSize[],int nprocess,int nblocks){
+
+    int allocation[nprocess];
+   // memset(allocation,-1,nprocess);
+   for(int i=0;i<nprocess;i++){
+    allocation[i]=-1;
+   }
+    for(int i=0;i<nprocess;i++){
+        for(int j=0;j<nblocks;j++){
+            if(processSize[i]<=blockSize[j]){
                 blockSize[j]=blockSize[j]-processSize[i];
                 allocation[i]=j;
                 break;
             }
         }
     }
-    for(int i=0;i<n;i++){
-            printf("process %d process size %d",i+1,processSize[i]);
-            if(allocation[i]==-1){
-                printf("not allocated\n");
-            }
-            else{
-                printf("allocated on %d\n",allocation[i]);
-            }
+    for(int i=0;i<nprocess;i++){
+        if(allocation[i]==-1){
+            printf("process%d--not allocated\n",i+1);
         }
+        else{
+            printf("process%d--%d\n",i+1,allocation[i]+1);
+        }
+       
+    }
 }
 int main(){
-    int n=5;
-     int blockSize[] = {100, 500, 200, 300, 600};
+    int blockSize[] = {100, 500, 200, 300, 600};
     int processSize[] = {212, 417, 112, 426};
-    firstfit(blockSize,processSize,n);
+    int nprocess=sizeof(processSize)/sizeof(processSize[0]);
+    int nblocks=sizeof(blockSize)/sizeof(blockSize[0]);
+    firstfit(blockSize,processSize,nprocess,nblocks);
+   
     return 0;
 }
